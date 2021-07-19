50e5e50e0d16470fd84e56871efac631a28149c3
ViliusJu <viliusj1@gmail.com>
2021-07-19 20:46:43 +0300

    index.html layout crated style. Add task funcktion, deleted tass function

---
+++ app.js
@@ -0,0 +1,95 @@
+
+
+
+
+
+/// Selectors
+
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listeners
+
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck)
+
+
+
+
+
+//Functions
+
+function addTodo(event){
+
+    //Prevent form from submitting
+
+    event.preventDefault();
+
+    //Todo Div
+
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    //Create list item
+
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+    //Completed button
+
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class = "fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton); 	
+
+     //Trash button
+
+     const trashButton = document.createElement('button');
+     trashButton.innerHTML = '<i class = "fas fa-trash"></i>';
+     trashButton.classList.add('trash-btn');
+     todoDiv.appendChild(trashButton); 
+
+     //Apend to list
+
+     todoList.appendChild(todoDiv);
+
+     //Clear input value
+
+     todoInput.value = "";
+
+    
+}
+
+
+function deleteCheck(event){
+    const item = event.target;
+
+    ///Delete buttons 
+
+    if(item.classList[0] === "trash-btn"){
+       const todo = item.parentElement;
+       //Animation
+       todo.classList.add("fall")
+       todo.addEventListener('transitionend', function(){
+           todo.remove();
+       })
+       
+    }
+
+    ///Chech button 
+
+    if (item.classList[0] === "complete-btn"){
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+
+}
+
+
+document.cookie = "username=John Doe; expires=Thu, 18 Dec 2025 12:00:00 UTC";
+
+
+
